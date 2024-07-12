c810e118ffc40828ed7f8687a9d3d1e84999e926
harshit yadav <harshityd2912002@gmail.com>
2024-07-13 00:48:56 +0530

    workout works come minor changes need to be done

--- gymBackend/models/workout.js
+++ gymBackend/models/workout.js
@@ -3,15 +3,26 @@ const mongoose  = require("mongoose") ;
 const WorkoutSchema = mongoose.Schema({
     name : {
         type : String ,
+        unique :true,
         required : true 
     },
     description : {
         type : String ,
         required : true 
     } ,
+    sessionTimeAvailability: [{
+        type :{type :String , required:true}, 
+        timing :{type : Number , required:true},
+        sessionDuration : {type :Number , default : 1}
+    }],
     instructors :{
         type : mongoose.Schema.ObjectId ,
         ref : "User" 
     }
 })
+
+// WorkoutSchema.pre("save" ,(next) =>{
+//    this.name = this.name.toUpperCase()  ;
+//    next() ;
+// })
 module.exports = mongoose.model('Workout', WorkoutSchema);
--- gymBackend/routes/api.js
+++ gymBackend/routes/api.js
@@ -1,9 +1,11 @@
 const express = require("express") ; 
-const {createData} = require("../controllers/Api")
+const {createData} = require("../controllers/Api");
+const {workoutCreated} =require("../controllers/workout") ;
 
 const router = express.Router() 
 
 // router.get("/create" , createData) ;
 router.get("/Create" , createData) ;
+router.post("/workout" , workoutCreated) ; 
 
 module.exports = router 
